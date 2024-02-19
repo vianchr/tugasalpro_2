34fd3140647dbe82705750efe2379ffa9dc979b2
vianchr <143708027+vianchr@users.noreply.github.com>
2024-02-20 00:14:50 +0700

    coba1

--- soal_2.py
+++ soal_2.py
@@ -2,5 +2,5 @@ def fungsi(x) :
     return 2*x**3 + 2*x + 15/x 
 x=int(input("masukan nilai x:"))
 hasil=fungsi(x)
-print (f"jadi nilai ketika x nya adalah {x} adalah {hasil}")
+print (f"jadi nilai fungsi(x) ketika x nya {x} adalah {hasil}")
 
