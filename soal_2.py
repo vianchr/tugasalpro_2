e748520f064e1a8a51b6a580e69215bb37e6dac5
vianchr <143708027+vianchr@users.noreply.github.com>
2024-02-20 18:26:48 +0700

    coba lagi

--- soal_2.py
+++ soal_2.py
@@ -1,6 +1,10 @@
-def fungsi(x) :
-    return 2*x**3 + 2*x + 15/x 
-x=int(input("masukan nilai x:"))
-hasil=fungsi(x)
-print (f"jadi nilai fungsi(x) ketika x nya {x} adalah {hasil}")
+while True :  
+    x=int(input("masukan nilai x:"))
+    if x == 0 :
+        print ("eror akibat terdapat bilangan yang dibagi dengan 0, cobalah bilangan bulat lain") 
+    else : 
+        hasil = 2*x**3 + 2*x + 15/x
+        print (f"jadi nilai fungsi(x) ketika x nya {x} adalah {hasil}")
+        break
+    
 
